1938516931cdd4b7f82e104c849d6242b7f5538d
Ignazio Secci <ignazio.secci@welld.ch>
2021-05-31 09:51:12 +0200

    section 2

--- section2/app.ts
+++ section2/app.ts
@@ -1,14 +1,73 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
+const MALE = 1;
+const FEMALE = 2;
+
+enum Gender { MALE, FEMALE }
+
+const person2: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number,string]; // tuple
+  gender: Gender; // enum
+} = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['sports', 'cooking'],
+  role: [2, 'author'],
+  gender: Gender.MALE,
+};
+
+// union types
+function combine(n1: (string | number), n2: (string | number)) {
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    return n1 + n2;
+  } else {
+    return n1.toString() + n2.toString();
+  }
+}
+
+const result1 = combine(1, 2);
+const result2 = combine('Max', 'Anna');
+
+
+// type aliases
+type Combinable = string | number;
+
+// literal types
+type Conversion = 'as-number' | 'as-text' // union type combined with literal types
+
+function combineWithLiteralTypes(
+  n1: Combinable, 
+  n2: Combinable,
+  resultConversion: Conversion
+): Combinable {
+  let result;
+  if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+    result = +n1 + +n2;
+  } else {
+    result = n1.toString() + n2.toString();
   }
   return result;
 }
 
-const number1 = 5;
-const number2 = 7.1;
-const printResult = true;
-const resultPhrase = "result is ";
+const result3 = combineWithLiteralTypes('1', 2, 'as-number');
+const result4 = combineWithLiteralTypes('Max', 'Anna', 'as-text');
 
-add(number1, number2, printResult, resultPhrase);
+// unknown type
+
+let something: unknown;
+let userName: string;
+
+something = 5;
+something = 'ok';
+if (typeof something === 'string') {
+  userName = something;
+}
+
+// never type
+
+function generateError(message: string, code: number): never { // never returns a value
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred', 500);
